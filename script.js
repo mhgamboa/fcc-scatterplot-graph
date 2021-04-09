a9442d8b39b0a058834fb3fa00b55f028bc112c0
Marcus <marcus.gamboa@gmail.com>
2021-04-09 12:14:05 -0600

    Added scatterplot and complete Axes

--- script.js
+++ script.js
@@ -3,32 +3,59 @@ const w = 900;
 const padding = 40;
 let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
 
+/* Fect Data */
 fetch(
   "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json"
 )
   .then((response) => response.json())
   .then((data) => {
-    const yearsArray = data.map((x) => new Date(x.Year));
-    console.log(yearsArray);
     /* Creating xAxis */
-    let minYear = d3.min(yearsArray, (d) => d);
-    let maxYear = d3.max(yearsArray, (d) => d);
-    // console.log(maYear);
-
     const xScale = d3
       .scaleLinear()
-      .domain([minYear, maxYear])
+      .domain([d3.min(data, (d) => d.Year - 1), d3.max(data, (d) => d.Year)])
       .range([padding, w - padding]);
 
-    const xAxis = d3.axisBottom(xScale);
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
 
     svg
       .append("g")
       .call(xAxis)
       .attr("transform", `translate(0, ${h - padding})`)
       .attr("id", "x-axis");
-    //CreateyAxis
-    /* plotDots*/
-    //Color dots
+
+    /* Create yAxis */
+    const yScale = d3
+      .scaleTime()
+      .domain([
+        d3.max(data, (d) => new Date(d.Seconds + 5) * 1000),
+        d3.min(data, (d) => new Date(d.Seconds) * 1000),
+      ])
+      .range([h - padding, padding]);
+
+    const yAxis = d3.axisLeft(yScale).tickFormat(d3.timeFormat("%M:%S"));
+
+    svg
+      .append("g")
+      .call(yAxis)
+      .attr("transform", `translate(${padding}, 0)`)
+      .attr("id", "y-axis");
+    /* Create Dots*/
+    svg
+      .selectAll("circle")
+      .data(data)
+      .enter()
+      .append("circle")
+      //Plot Dots
+      .attr("cx", (d) => xScale(d.Year))
+      .attr("cy", (d) => yScale(d.Seconds * 1000))
+      .attr("r", 5)
+      //Add Proper Labels to Dots
+      .attr("class", "dot")
+      .attr("data-xvalue", (d) => d.Year)
+      .attr("data-yvalue", (d) => new Date(d.Seconds * 1000))
+      //Color dots
+      .attr("stroke", "black")
+      .attr("stroke-width", 1)
+      .attr("fill", (d) => (d.Doping ? "red" : "green"));
     // Add ToolTips to dots
   });
