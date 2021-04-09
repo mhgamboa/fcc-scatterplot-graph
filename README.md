f4569a3991ce4934a4a3306dd2fbf528f64e414f
Marcus <marcus.gamboa@gmail.com>
2021-04-09 14:48:27 -0600

    ALL TESTS PASS

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # fcc-scatterplot-graph
+
+Had difficulty Scaling the Time, but I was able to learn that date objects work better with milliseconds over seconds, and was able to generate to correct times.
+
+I also learned how to better work with the `.on('event', function(event, datum))`. I didn't know the function takes two parameters, which makes my life a lot easier when trying to acess the information.
+
+I also learned about the `selection.html()` which I used over the `selection.text()` in the tooltip.
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const h = 600;
+const h = 560;
 const w = 900;
 const padding = 40;
 let svg = d3.select("body").append("svg").attr("width", w).attr("height", h);
+const tooltip = d3.select("#tooltip");
 
 /* Fect Data */
 fetch(
@@ -27,8 +28,8 @@ fetch(
     const yScale = d3
       .scaleTime()
       .domain([
-        d3.max(data, (d) => new Date(d.Seconds + 5) * 1000),
-        d3.min(data, (d) => new Date(d.Seconds) * 1000),
+        d3.max(data, (d) => new Date(d.Seconds * 1000 + 5000)),
+        d3.min(data, (d) => new Date(d.Seconds * 1000)),
       ])
       .range([h - padding, padding]);
 
@@ -39,6 +40,7 @@ fetch(
       .call(yAxis)
       .attr("transform", `translate(${padding}, 0)`)
       .attr("id", "y-axis");
+
     /* Create Dots*/
     svg
       .selectAll("circle")
@@ -56,6 +58,69 @@ fetch(
       //Color dots
       .attr("stroke", "black")
       .attr("stroke-width", 1)
-      .attr("fill", (d) => (d.Doping ? "red" : "green"));
-    // Add ToolTips to dots
+      .attr("fill", (d) => (d.Doping ? "red" : "blue"))
+      // Add ToolTips to dots
+      .on("mouseover", (e, d) => {
+        tooltip
+          .style("visibility", "visible")
+          .style("left", e.pageX - 25 + "px")
+          .style("top", e.pageY - 90 + "px")
+          .attr("data-year", d.Year)
+          .html(
+            `${d.Name}: ${d.Nationality}<br/>Year: ${d.Year}<br/>Time: ${d.Time}`
+          );
+      })
+      .on("mousemove", (e, d) => {
+        tooltip
+          .style("visibility", "visible")
+          .style("left", e.pageX - 25 + "px")
+          .style("top", e.pageY - 90 + "px")
+          .attr("data-year", d.Year)
+          .html(
+            `${d.Name}: ${d.Nationality}<br/>Year: ${d.Year}<br/>Time: ${d.Time}`
+          );
+      })
+      .on("mouseout", (e, d) => {
+        tooltip.transition().style("visibility", "hidden");
+      });
+    /* Create Legend */
+    const legendPositionX = w - 320;
+    const legendPositionY = padding;
+    const legend = svg.append("g");
+    const legendContainer = legend
+      .append("rect")
+      .attr("id", "legend")
+      .attr("width", 220)
+      .attr("height", 100)
+      .attr("x", legendPositionX)
+      .attr("y", legendPositionY)
+      .attr("fill", "none");
+
+    const dopingSquare = legend
+      .append("rect")
+      .attr("x", legendPositionX + 10)
+      .attr("width", 25)
+      .attr("height", 25)
+      .attr("y", legendPositionY + 8)
+      .attr("fill", "red");
+
+    const dopingText = legend
+      .append("text")
+      .attr("x", legendPositionX + 50)
+      .attr("y", legendPositionY + 25)
+      .text("Doping Allegation");
+
+    const notDopingSquare = legend
+      .append("rect")
+      .attr("x", legendPositionX + 10)
+      .attr("width", 25)
+      .attr("height", 25)
+      .attr("y", legendPositionY + 51)
+      .attr("fill", "blue");
+
+    const notDopingText = legend
+      .append("text")
+      .attr("x", legendPositionX + 50)
+      .attr("y", legendPositionY + 70)
+      .text("No Doping Allegation");
   });
